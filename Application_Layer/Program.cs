119251066fbc22967280c3c47cda347147b4896b
rayde <raydenilsonsantacruz@gmail.com>
2023-05-14 14:56:32 -0400

    useCors added

--- Application_Layer/Program.cs
+++ Application_Layer/Program.cs
@@ -38,6 +38,7 @@ if (app.Environment.IsDevelopment())
 else
 {
     app.UseExceptionHandler("/Error");
+    app.UseCors(policy => policy.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin());
     app.UseHsts();
 }
 
